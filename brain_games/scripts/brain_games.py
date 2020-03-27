4f365540eac71489c8c92dc1d501849c82a2b419
Odintsov Timur <timodin@yandex.ru>
2020-03-27 14:22:24 +0300

    brain_games: add dockstring

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,10 +4,11 @@ from brain_games import cli
 
 
 def main():
+    '''main projects func'''
     print('Welcome to the Brain Games!')
     print()
     cli.welcome_user()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
