1674fbc126d402a60851a7d75e30848ef2a09179
Odintsov Timur <timodin@yandex.ru>
2020-03-27 18:49:09 +0300

    cli: add welcome_user_to_even_game() func

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -12,3 +12,12 @@ def welcome_user():
 def greet_user_by_name(name):
     '''Greets user by his name'''
     print(f'Hello, {name}!')
+
+
+def welcome_user_to_even_game():
+    '''Greets and asks for a name of username and save it.'''
+    print('Welcome to the Brain Games!')
+    print('Answer "yes" if number even otherwise answer "no".')
+    print()
+    name = prompt.string('May I have your name? ')
+    return name
