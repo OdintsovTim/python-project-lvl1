3b31fec40cf8617ff75329510f3822d4c620d1c1
Odintsov Timur <timodin@yandex.ru>
2020-03-27 18:46:32 +0300

    game_logic: add make_even_game() func

---
+++ brain_games/games_logic.py
@@ -0,0 +1,32 @@
+import random
+
+
+def make_even_game(name):
+    '''logic of even game'''
+    correct_answers_counter = 0
+    min_number = - 1000
+    max_number = 1000
+
+    while correct_answers_counter < 3:
+        random_number = random.randint(min_number, max_number)
+
+        if random_number % 2 == 0:
+            correct_answer = 'yes'
+        else:
+            correct_answer = 'no'
+
+        print(f'Question: {random_number}')
+        users_answer = input('Your answer: ').lower()
+
+        if users_answer == correct_answer:
+            print('Correct!')
+            correct_answers_counter += 1
+        else:
+            print(
+                '"{0}" is wrong answer ;(. Correct answer was "{1}".'.format(
+                    users_answer, correct_answer
+                )
+            )
+            print(f'Let\'s try again, {name}')
+
+    print(f'Congratulations, {name}!')
