94099b4203c88272cd362f4d0579b35deb0dca5b
Odintsov Timur <timodin@yandex.ru>
2020-03-28 13:51:48 +0300

    cli: add description for gcd game

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,7 @@
 import prompt
 
 
-def welcome_user(even=False, calc=False):
+def welcome_user(even=False, calc=False, gcd=False):
     '''Greets and asks for a name of username and save it.'''
     print('Welcome to the Brain Games!')
 
@@ -9,6 +9,8 @@ def welcome_user(even=False, calc=False):
         print('Answer "yes" if number even otherwise answer "no".')
     elif calc:
         print('What is the result of the expression?')
+    elif gcd:
+        print('Find the greatest common divisor of given numbers.')
     print()
 
     name = prompt.string('May I have your name? ')
