1910d6156ae1776ff5c5a53e69231e9cf6596b32
Odintsov Timur <timodin@yandex.ru>
2020-03-28 13:52:31 +0300

    brain_gcd: add file and main() func

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from brain_games import cli, games_logic
+
+
+def main():
+    '''Scripts main func.'''
+    name = cli.welcome_user(gcd=True)
+    games_logic.make_game(name, gcd=True)
+
+
+if __name__ == '__main__':
+    main()
