82f0f0b372e50b116cf1372a9c20302175b6ef17
Odintsov Timur <timodin@yandex.ru>
2020-03-28 03:59:33 +0300

    brain_calc: add main() func

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from brain_games import cli, games_logic
+
+
+def main():
+    '''Scripts main func.'''
+    name = cli.welcome_user(calc=True)
+    games_logic.make_game(name, calc=True)
+
+
+if __name__ == '__main__':
+    main()
