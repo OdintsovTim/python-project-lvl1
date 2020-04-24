499be33f6394676faba171065b8bffbbbe22d6a9
Odintsov Timur <timodin@yandex.ru>
2020-04-24 13:48:42 +0300

    games.calc: fix round() func

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -4,11 +4,7 @@ import random
 
 MAX_NUMBER = 100
 MIN_NUMBER = -100
-OPERATIONS = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-}
+OPERATIONS = [('+', operator.add), ('-', operator.sub), ('*', operator.mul)]
 DESCRIPTION = 'What is the result of the expression?'
 
 
@@ -16,11 +12,10 @@ def round():
     '''Returns question and correct answer for calc game.'''
     first_random_number = random.randint(MIN_NUMBER, MAX_NUMBER)
     second_random_number = random.randint(MIN_NUMBER, MAX_NUMBER)
-    operation = random.choice(list(OPERATIONS.keys()))
+    symbol, function = random.choice(OPERATIONS)
 
-    correct_answer = str(OPERATIONS[operation](first_random_number,
-                                               second_random_number))
-    question = '{0} {1} {2}'.format(first_random_number, operation,
+    correct_answer = str(function(first_random_number, second_random_number))
+    question = '{0} {1} {2}'.format(first_random_number, symbol,
                                     second_random_number)
 
     return question, correct_answer
